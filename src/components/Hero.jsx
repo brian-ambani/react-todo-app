ad7358946bcb0652a739e3c2f28e8144e79f9d26
brian-ambani <brainambani1@gmail.com>
2023-09-07 03:16:08 +0300

    updated the components

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Hero = () => {
+  return (
+    <div>
+        <Nav />
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <ul>
+            <li>All Tasks</li>
+            <li>Incomplete</li>
+            <li>Completed</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
